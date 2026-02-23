7d4e1fbe41c423f02e0b52ec1243dcd56b4bf186
Yejoong (Paul) Kim <yejoongpaulk@gmail.com>
2026-02-22 21:52:28 -0500

    Added explicit references to repositories and entities

--- src/main/java/dev/yejoongpaulk/sblreader/SblreaderApplication.java
+++ src/main/java/dev/yejoongpaulk/sblreader/SblreaderApplication.java
@@ -2,8 +2,12 @@ package dev.yejoongpaulk.sblreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "dev.yejoongpaulk.sblreader.verse")
+@EntityScan(basePackages = "dev.yejoongpaulk.sblreader.verse")
 public class SblreaderApplication {
 
 	public static void main(String[] args) {
